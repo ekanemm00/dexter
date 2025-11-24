06282df3e7e834769bfbb2a3ee0a3ea05830d671
ekanemm00 <ekanemm00@gmail.com>
2025-11-24 13:16:32 +0100

    Initial commit

---
+++ app/auth/login/page.js
@@ -0,0 +1,44 @@
+import { TextField } from "@mui/material"
+import { FcGoogle } from "react-icons/fc";
+import { FaGithub } from "react-icons/fa";
+
+export default function Login(){
+    return(
+        <main className="min-h-screen flex justify-center px-2 md:px-12 md:py-10 lg:px-16 lg:py-12">
+            <div className="w-full md:w-[350px] max-h-[400px] flex flex-col gap-8 rounded md:shadow-md md:px-3 md:py-4">
+                <div>
+                    <h1 className="text-3xl font-semibond text-center">Log in</h1>
+                    <p className="text-blue-300 text-center">Create an Account or Sign in</p>
+                </div>
+                <form className="justify-item-center">
+                    <div className="w-full mb-2">
+                        <TextField
+                        fullWidth
+                        placeholder="ekanem@gmail.com"
+                        label="Enter Email"
+                        type="email"
+                        id="email"
+                        size="small"
+                        />
+                        <button type="submit" className="w-full text-white h-10 rounded-md bg-blue-500 hover:opacity-40 p-3 mt-2">Continue</button>
+                    </div>
+                </form>
+                <p className="text-center text-gray-700">or sign up with</p>
+                <div className="flex justify-center gap-4">
+                <form className="justify-item-center">
+                    <button type="submit" className="w-10 h-10 border rounded-md shadow-lg border-gray-300 cursor-pointer">
+                        <FcGoogle className="text-4xl"/>
+                    </button>
+                </form>
+                <form>
+                    <button type="submit" className="w-10 h-10 border rounded-md shadow-lg border-gray-300 cursor-pointer">
+                        <FaGithub className="text-4xl"/>
+                    </button>
+                </form>
+                </div>
+
+            </div>
+        </main>
+        
+    )
+}
--- app/page.js
+++ app/page.js
@@ -1,7 +1,37 @@
-import Image from "next/image";
+"use client"
+import {  Box, Typography, Button, Card, CardContent } from "@mui/material";
 
-export default function Home() {
+
+export default function Home(){
   return (
-   <div>WELCOME TO MY LANDING PAGE</div>
-  );
+    <Box sx={{minHeight: "100vh",
+      backgroundColor: "#ffd700",
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      padding: 2
+    }}>
+      <Card sx={{maxWidth: 480, padding: 3, transition:"0.3s", "&:hover":{transform:"scale(1.02)", boxShadow:5}}}>
+        <CardContent>
+          <Typography variant="h4" fontWeight="bold" gutterBottom>
+            Welcome to my homepage
+          </Typography>
+
+          <Typography variant="body1" sx={{marginBlock: 2}}>
+            I'm a Web Developer passionate about building modern and simple user interface.
+          </Typography>
+
+          <a 
+          href="https://www.linkedin.com/in/michael-ekanem-917241301"
+          target="_blank">
+          <Button variant="contained" size="large" fullWidth sx={{mt: 2}}>
+            Visit My linkedin Lets Get Started
+          </Button>
+          </a>
+
+        </CardContent>
+
+      </Card>
+    </Box>
+  )
 }
